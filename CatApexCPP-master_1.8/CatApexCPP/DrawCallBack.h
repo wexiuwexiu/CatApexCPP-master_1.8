58beaf8b3385f4fc867e32de02ade080a20b94ca
wexiuwexiu <48826894+wexiuwexiu@users.noreply.github.com>
2021-02-12 22:06:25 +0800

    Add files via upload

---
+++ CatApexCPP-master_1.8/CatApexCPP/DrawCallBack.h
@@ -0,0 +1,10 @@
+//
+// Created by Administrator on 2019/6/20.
+//
+
+#ifndef CATAPEXCPP_DRAWCALLBACK_H
+#define CATAPEXCPP_DRAWCALLBACK_H
+
+#endif //CATAPEXCPP_DRAWCALLBACK_H
+#define myFontSize			16  //×ÖÌå´óÐ¡
+void draw();
